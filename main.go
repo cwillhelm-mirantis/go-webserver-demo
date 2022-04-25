bdfcbaf3f09be3fbe3a9630d4e7174b5896194cc
Connor Willhelm <cwillhelm@cwillhelm-MBP14-1966.local>
2022-04-25 09:28:39 -0400

    1.0.6

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func sayhello(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, test -- we're on branch 1.0.5")
+    fmt.Fprintf(w, "Hello, test -- we're on branch 1.0.6")
 }
 
 func main() {
